1667ed877b832f45732fcc858a053977a7e20904
Akram Saouri <akram_saouri@hotmail.fr>
2016-07-08 21:04:07 +0000

    open browser + rename file

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const path = require('path'),
     express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-
     commander = require('commander'),
     package_json = require(path.join(__dirname, 'package.json'));
+open = require('open');
 
 // command line utility
 commander
     .version(package_json.version)
     .option('--port <port>', 'Port to listen to (3000 default one)', parseInt)
+    .option('--browser <browser>', 'Favorite ex [chrome, firefox, iexplore,opera]')
     .parse(process.argv);
 
 const port = commander.port || 3000,
+    browser = commander.browser || 'chrome',
 
     explore = require('./explorer'); // explorer module
 
@@ -31,4 +33,7 @@ app.get('/explore/unlink', explore.unlink);
 
 app.post('/explore/rename', explore.rename);
 
-app.listen(port); //listen on the chosen port ( 3000 default one )
+app.listen(port,()=> console.log(`starting app on ${port} port with ${browser} browser`)); //listen on the chosen port ( 3000 default one )
+
+// serve file in the browser
+open(`http://localhost:${port}`, browser);
